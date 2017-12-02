8517d67fc1d151763c63e1e6f26fd07e91a2eb0b
glennfield20 <ladesmaglenngarfield@gmail.com>
2017-12-02 16:15:51 +0800

    Adds associations.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < User
+  has_many :subject_enrolleds
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,5 @@
 class Subject < ApplicationRecord
   belongs_to :instructor
   has_many :exams
+  has_many :subject_enrolleds
 end
